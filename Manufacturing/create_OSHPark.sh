8ff72a9a469c9dd797f58b10528534b192945d6b
Bruce Lacey <bruce@blacey.com>
2015-06-25 10:03:03 -0700

    Added a create_OSHPark.sh Gerber generation script to Manufacturing directory for quick verification of Gerbers and for those users who might prefer to use OSHPark for fabbing.

--- Manufacturing/create_OSHPark.sh
+++ Manufacturing/create_OSHPark.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-#  http://smart-prototyping.com/Prototyping-Services/Electronic-Prototyping/PCB-Prototyping.html
+#  https://www.oshpark.com/guidelines
 
-fabHouse="SmartPrototypes"
+fabHouse="OSHPark"
 kiprjFile="${file##../*.pro/}"
 sourceName="${kiprjFile%.*}"
 gerberName="MySensorsIRBlaster"
@@ -10,15 +10,13 @@ archive="${gerberName}-${fabHouse}-Gerbers.zip"
 
 [ -e "${archive}" ] && rm "${archive}" 
 
-cp *.drl           ${gerberName}-BRD_DRILL.TXT  	# Drill & Holes 	[.TXT]
-cp *-Edge_Cuts.gbr ${gerberName}-BRD_EDGE.GML		# Outline		[.GML]
-cp *-F_Paste.gtp   ${gerberName}-TOP_PASTE.GTP		# Solder Paste Top	[.GTP]
-cp *-B_Paste.gbp   ${gerberName}-BTM_PASTE.GBP		# Solder Paste Bottom	[.GBP]
+cp *.drl           ${gerberName}-BRD_DRILL.XLN  	# Drill & Holes 	[.XLN]
+cp *-Edge_Cuts.gbr ${gerberName}-BRD_EDGE.GKO		# Outline		[.GKO]
 cp *-F_Mask.gts    ${gerberName}-TOP_MASK.GTS		# Solder Mask Top	[.GTS]
 cp *-B_Mask.gbs    ${gerberName}-BTM_MASK.GBS		# Solder Mask Bottom	[.GBS]
 cp *-F_SilkS.gto   ${gerberName}-TOP_SILK.GTO		# Silk Screen Top	[.GTO]
 cp *-B_SilkS.gbo   ${gerberName}-BTM_SILK.GBO		# Silk Screen Bottom	[.GBO]
-cp *-F_Cu.gtl      ${gerberName}-TOP_COPPER.GTL		# Top Layer		[.GTL]
+cp *-F_Cu.gtl 	   ${gerberName}-TOP_COPPER.GTL		# Top Layer		[.GTL]
 cp *-B_Cu.gbl 	   ${gerberName}-BTM_COPPER.GBL		# Bottom Layer		[.GBL]
 
-zip -m ${archive} ${gerberName}-*.{TXT,GML,GTP,GBP,GTS,GBS,GTO,GBO,GTL,GBL} 
+zip -m ${archive} ${gerberName}-*.{GTL,GBL,GTS,GBS,GTO,GBO,GKO,XLN} 
